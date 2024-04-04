4e0173f6e0f04bf5848647dc305b9152d97cdee7
kineticwallet <105553638+kineticwallet@users.noreply.github.com>
2024-04-04 23:46:18 +0200

    add lemon-signal-counter

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import LemonSignal from "@rbxts/lemon-signal";
+import type { Signal, SignalCallback } from "@rbxts/lemon-signal/dist/LemonSignal";
+
+export class CountedLemonSignal<T> {
+	private Signal: Signal<T>;
+	private OnConnectionsChanged = new LemonSignal<number>();
+	private TotalConnections = 0;
+
+	public constructor(signal: Signal<T>) {
+		this.Signal = signal;
+	}
+
+	public Connect(fn: SignalCallback<T>): Signal.Connection {
+		this.TotalConnections++;
+		this.OnConnectionsChanged.Fire(1);
+		return this.Signal.Connect(fn);
+	}
+
+	public DisconnectAll(): void {
+		this.OnConnectionsChanged.Fire(-this.TotalConnections);
+		this.TotalConnections = 0;
+		this.Signal.DisconnectAll();
+	}
+
+	public Disconnect(connection: Signal.Connection): void {
+		this.TotalConnections--;
+		this.OnConnectionsChanged.Fire(-1);
+		connection.Disconnect();
+	}
+
+	public Reconnect(connection: Signal.Connection): void {
+		this.TotalConnections++;
+		this.OnConnectionsChanged.Fire(1);
+		connection.Reconnect();
+	}
+
+	public Destroy(): void {
+		this.OnConnectionsChanged.Destroy();
+		table.clear(this);
+		setmetatable(this, undefined!);
+	}
+}
